519e16dce4ddc65eac87939b1afbc62e2e54d40d
jonathan-joel <jonathanjoelcepeda@gmail.com>
2023-11-09 22:45:27 -1200

    analyzing the code in App.js to work on the components of the application

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet,Platform, Text, View, Button, SafeAreaView } from 'react-native';
+import {useState} from "react";
+const colors = ["#F7DC6F", "#A2D9CE", "#D7BDE2"];
 
 export default function App() {
+  const [isWorking, setIsworking]= useState(false);
+  const [time, setTime]= useState(25 * 60);
+  const [currentTime, setCurrentTime]= useState("POMO" | "SHORT" | "BREAK");
+
+
   return (
-    <View style={styles.container}>
-      <Text>pomodoro</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}> 
+      <View style={{paddingTop: Platform.os === "android" && 30}}>
+        <Text style={styles.text}>Pomodoro</Text> 
+        <Text style={styles.text}>{time}</Text>      
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+},
 });
