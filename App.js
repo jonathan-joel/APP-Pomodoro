87f110c07ff087078872962097bd42a3fc1bea5e
jonathan-joel <jonathanjoelcepeda@gmail.com>
2023-11-10 00:17:03 -1200

    creating the Header.js component to contain our taps and it will show us the clock

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
-
 import { StyleSheet,Platform, Text, View, Button, SafeAreaView } from 'react-native';
 import {useState} from "react";
+import Header from './src/components/Header';
+
 const colors = ["#F7DC6F", "#A2D9CE", "#D7BDE2"];
 
 export default function App() {
@@ -8,12 +9,14 @@ export default function App() {
   const [time, setTime]= useState(25 * 60);
   const [currentTime, setCurrentTime]= useState("POMO" | "SHORT" | "BREAK");
 
-
-  return (
-    <SafeAreaView style={styles.container}> 
+ return (
+    <SafeAreaView style={[styles.container, {backgroundColor: colors[currentTime]}]}> 
       <View style={{paddingTop: Platform.os === "android" && 30}}>
         <Text style={styles.text}>Pomodoro</Text> 
-        <Text style={styles.text}>{time}</Text>      
+        <Text style={styles.text}>{time}</Text> 
+        <Header currentTime={currentTime} 
+                setCurrentTime={setCurrentTime}
+                setTime={setTime}/>     
       </View>
     </SafeAreaView>
   );
